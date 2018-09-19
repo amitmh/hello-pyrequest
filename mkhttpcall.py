b3a514fb31b9d6c5d51b68613d419519c44101d7
Amit Mhatre <amit.mhatre@hotmail.com>
2018-09-19 19:25:11 +0100

    added DEF main try

--- mkhttpcall.py
+++ mkhttpcall.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 import requests
 
-headers = {'user-agent': 'my-app/0.0.1', 'Accept': 'application/xml'}
-proxies = {'http://host.name': 'foo.bar:4012'}
+DEF_URI='http://docs.python-requests.org/en/master/api/'
 
-r = requests.request('GET',
-                     'http://docs.python-requests.org/en/master/api/',
-                     allow_redirects=False,
-                     proxies=proxies,
-                     headers=headers)
+def main():
+    headers={'user-agent': 'my-app/0.0.1', 'Accept': 'application/xml'}
+    proxies={'http://host.name': 'foo.bar:4012'}
 
-print(r.text)
-print(r.status_code)
-print(r.headers)
+    r = requests.request('GET',
+                         DEF_URI,
+                         proxies=proxies,
+                         headers=headers)
+
+    #print(r.text)
+    print(r.status_code)
+    print(r.headers)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as ex:
+        raise
+        sys.exit(ex)
+        
