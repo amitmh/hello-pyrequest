5c4025672f3f3bab26cf3efc6f577b08e7af76cb
Amit Mhatre <amit.mhatre@hotmail.com>
2018-09-19 19:31:57 +0100

    improved formatting and error log

--- mkhttpcall.py
+++ mkhttpcall.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 import requests
+import sys
 
-DEF_URI='http://docs.python-requests1.org/en/master/api/'
+DEF_URI = 'http://docs.python-requests.org/en/master/api/'
+
 
 def main():
-    headers={'user-agent': 'my-app/0.0.1', 'Accept': 'application/xml'}
-    proxies={'http://host.name': 'foo.bar:4012'}
+    headers = {'user-agent': 'my-app/0.0.1', 'Accept': 'application/xml'}
+    proxies = {'http://host.name': 'foo.bar:4012'}
 
     r = requests.request('GET',
                          DEF_URI,
                          proxies=proxies,
                          headers=headers)
 
-    #print(r.text)
+    # print(r.text)
     print(r.status_code)
     print(r.headers)
 
-if __name__ == "__main__":
-    main()
 
-        
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as ex:
+        #raise
+        sys.exit(ex)
+
